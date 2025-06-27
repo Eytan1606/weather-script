7d191dda505813767b958753934aace31d419edb
Fielding27 <Fhailyandro4@gmail.com>
2025-06-27 18:25:51 +0300

    initial commit

---
+++ batterystatus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+INTERNAL=$(cat /sys/class/power_supply/BAT0/capacity)
+EXTERNAL=$(cat /sys/class/power_supply/BAT1/capacity)
+STATUS=$(cat /sys/class/power_supply/BAT1/status)
+
+echo "$INTERNAL $EXTERNAL $STATUS"
