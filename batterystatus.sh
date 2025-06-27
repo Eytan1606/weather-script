2f90d1cfaf8c6f627b124b92de45915357029b7a
Fielding27 <Fhailyandro4@gmail.com>
2025-06-27 19:11:26 +0300

    c-#	modified:   batterystatus.sh

--- batterystatus.sh
+++ batterystatus.sh
@@ -4,4 +4,4 @@ INTERNAL=$(cat /sys/class/power_supply/BAT0/capacity)
 EXTERNAL=$(cat /sys/class/power_supply/BAT1/capacity)
 STATUS=$(cat /sys/class/power_supply/BAT1/status)
 
-echo "$INTERNAL $EXTERNAL $STATUS"
+echo "$INTERNAL $EXTERNAL $STATU"
