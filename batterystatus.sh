56e9274ad206847967302e7d369980d77b097921
Fielding27 <fhailyandro4@gmail.com>
2025-06-30 22:15:01 +0300

    Delete batterystatus.sh

--- batterystatus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-INTERNAL=$(cat /sys/class/power_supply/BAT0/capacity)
-EXTERNAL=$(cat /sys/class/power_supply/BAT1/capacity)
-STATUS=$(cat /sys/class/power_supply/BAT1/status)
-
-echo "$INTERNAL $EXTERNAL $STATU"
